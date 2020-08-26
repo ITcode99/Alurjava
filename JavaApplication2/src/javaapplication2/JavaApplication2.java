7818c739bbba72d029fee1e88a6b02fc36463b46
ITcode99 <70245061+ITcode99@users.noreply.github.com>
2020-08-26 18:29:52 +0700

    Add files via upload

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Adi Arput
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nama Siswa : Adi Putra");
+        System.out.println("Jenis Kelamin : Laki-laki");
+        System.out.println("Alamat : Jl.Masjid Perahu Ds.Bakalan");
+        System.out.println("Jurusan : Rekayasa Perangkat Lunak");
+    }
+    
+}
